34debdb46234ca878479a23449251b50e1e87845
Gavin Cobb <gavin.cobb@hillrom.com>
2022-12-02 10:03:28 -0500

    Add project files.

---
+++ Controllers/IndexController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Portfolio.Models;
+using System.Diagnostics;
+
+namespace Portfolio.Controllers
+{
+    public class IndexController : Controller
+    {
+        private readonly ILogger<IndexController> _logger;
+
+        public IndexController(ILogger<IndexController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Portfolio()
+        {
+            return View();
+        }
+        public IActionResult Resume()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
